f410ae36e6e9a4d5a724add11adaef881a768187
kuria-k <kelvinmacharia442@gmail.com>
2025-11-26 19:42:06 +0300

    Admin changes

--- front-end/src/routes/adminroutes.jsx
+++ front-end/src/routes/adminroutes.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import AdminLayout from "../layouts/adminlayout";
-import Dashboard from "../pages/admin/Dashboard";
+import Dashboard from "../pages/admin/dashboard";
 import Users from "../pages/admin/users";
-import Branches from "../pages/admin/Branches";
+import Branches from "../pages/admin/branches";
 
 const AdminRoutes = () => (
   <Routes>
